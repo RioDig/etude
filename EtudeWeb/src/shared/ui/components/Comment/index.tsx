22162bf331032ff13a4ea728c97c259e7c46f247
Aleksey Vorsin <leha.vorsin@mail.ru>
2024-09-18 20:44:22 +0500

    Добавлен компонент коммент

---
+++ EtudeWeb/src/shared/ui/components/Comment/index.tsx
@@ -0,0 +1,55 @@
+import cn from "classnames";
+import styles from './styles.module.scss';
+
+export enum CommentType {
+  accept = 'accept',
+  deny = 'deny',
+}
+
+interface IComment {
+  type?: CommentType;
+  title: string;
+  fullName: string;
+  position?: string;
+  commentText: string;
+  className?: string;
+}
+
+export const Comment = ({
+  type = CommentType.accept,
+  title,
+  fullName,
+  position,
+  commentText,
+  className,
+}: IComment) => {
+  const commentClassName = cn(
+    styles.root,
+    {
+      [styles.accept]: type === CommentType.accept,
+      [styles.deny]: type === CommentType.deny,
+    },
+    className,
+  )
+
+  return (
+    <div className={commentClassName}>
+      <div className={"flex flex-col items-start text-start"}>
+        <div className={`font-bold text-base ${styles.commentTitle}`}>
+          {title}
+        </div>
+        <div className={"font-semibold text-base flex gap-1"}>
+          {fullName}
+          {position &&
+            <span className={"font-normal text-gray-300 text-base"}>
+              {position}
+            </span>
+          }
+        </div>
+      </div>
+      <p className={"font-normal text-gray-900 text-base text-start"}>
+        {commentText}
+      </p>
+    </div>
+  )
+}
