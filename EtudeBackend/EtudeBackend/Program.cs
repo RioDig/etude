7eba10b102645f5ad958280b18e2c19017c80ba5
KekJoy <valiullin.anton2003@yandex.ru>
2025-04-28 11:56:35 +0500

    fix CORS

--- EtudeBackend/EtudeBackend/Program.cs
+++ EtudeBackend/EtudeBackend/Program.cs
@@ -72,9 +72,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.WithOrigins("http://localhost:5173", "https://localhost:5173", "*")
             .AllowAnyMethod()
-            .AllowAnyHeader();
+            .AllowAnyHeader()
+            .AllowCredentials();
     });
 });
 
