06c00164c0451c8cf0525fe65eb0dbb20764d801
KekJoy <valiullin.anton2003@yandex.ru>
2025-05-04 03:54:27 +0500

    fix Autocomplete.cs

--- EtudeBackend/EtudeBackend/Features/Users/Controllers/UserController.cs
+++ EtudeBackend/EtudeBackend/Features/Users/Controllers/UserController.cs
@@ -1,7 +1,5 @@
 ﻿using System.Security.Claims;
-using EtudeBackend.Features.Users.DTOs;
 using EtudeBackend.Features.Users.Services;
-using Microsoft.AspNetCore.Authentication.BearerToken;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -66,27 +64,5 @@ public class UserController : ControllerBase
             return Unauthorized();
             
         return Ok(user);
-    }
-    
-    /// <summary>
-    /// Получает список сотрудников для автокомплита
-    /// </summary>
-    /// <param name="term">Поисковый запрос для фильтрации сотрудников</param>
-    /// <param name="idsToRemove">Массив идентификаторов сотрудников, которых нужно исключить из результатов</param>
-    /// <returns>Список сотрудников и флаг наличия дополнительных элементов</returns>
-    [HttpGet("employee")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    public async Task<IActionResult> GetEmployeeAutocomplete(
-        [FromQuery] string? term, 
-        [FromQuery] string[]? idsToRemove)
-    {
-        var (employees, hasMoreItems) = await _userService.GetAutocompleteEmployeesAsync(term, idsToRemove);
-        
-        return Ok(new EmployeeAutocompleteResponse
-        {
-            Employees = employees,
-            HasMoreItems = hasMoreItems
-        });
     }
 }
---
+++ EtudeBackend/EtudeBackend/Shared/Extensions/Autocomplete.cs
@@ -0,0 +1,41 @@
+﻿using EtudeBackend.Features.Users.DTOs;
+using EtudeBackend.Features.Users.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EtudeBackend.Shared.Extensions;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class AutocompleteController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public AutocompleteController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    /// <summary>
+    /// Получает список сотрудников для автокомплита
+    /// </summary>
+    /// <param name="term">Поисковый запрос для фильтрации сотрудников</param>
+    /// <param name="idsToRemove">Массив идентификаторов сотрудников, которых нужно исключить из результатов</param>
+    /// <returns>Список сотрудников и флаг наличия дополнительных элементов</returns>
+    [HttpGet("employee")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> GetEmployeeAutocomplete(
+        [FromQuery] string? term, 
+        [FromQuery] string[]? idsToRemove)
+    {
+        var (employees, hasMoreItems) = await _userService.GetAutocompleteEmployeesAsync(term, idsToRemove);
+        
+        return Ok(new EmployeeAutocompleteResponse
+        {
+            Employees = employees,
+            HasMoreItems = hasMoreItems
+        });
+    }
+}
