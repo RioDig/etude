e0e91a7d8e11b24e7660b239228bab7edb09de36
Aleksey Vorsin <leha.vorsin@mail.ru>
2025-05-18 23:12:45 +0500

    change api url

--- EtudeWeb/src/shared/config.ts
+++ EtudeWeb/src/shared/config.ts
@@ -6,4 +6,4 @@ export const env: EnvType = {
   WEB_API_URL: import.meta.env.VITE_WEB_API_URL
 }
 
-export const API_URL = 'http://localhost:5055/api'
+export const API_URL = 'http://109.73.203.57/api'
